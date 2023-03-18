ded02796ba0bf46a5bfd194ee4890f37b8516197
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-03-18 16:57:11 -0500

    Se finalizo la seccion del header, html, css y js para el cuadro de busqueda

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 import './style.css'
 
-const Api_Key = '';
+'use strict';
 
-const APikey= '';
+/**
+ * Agregar eventos en multiples elementos
+ */
+
+const addEventonElements = (elementos, TipodeEvento, callback) =>{
+    for(const elem of elementos){
+        elem.addEventListener(TipodeEvento, callback);
+    }
+}
+
+/**
+ * Alternar el cuadro de busqueda en el dispositivo movil || small screen
+*/
+
+const searchBox = document.querySelector('[search-box]');
+const searchTogglers = document.querySelectorAll('[search-toggler]');
+
+addEventonElements(searchTogglers, 'click', () =>{
+    searchBox.classList.toggle('active');
+})
+
+
+
+
