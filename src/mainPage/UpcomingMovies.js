d85bbfa087ac9a3eab7e176a58c2761e097de3b9
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-04-07 00:27:54 -0500

    Se agrego un archivo Utils.js para reutilizar codigo

--- src/mainPage/UpcomingMovies.js
+++ src/mainPage/UpcomingMovies.js
@@ -1,3 +1,5 @@
+import { createMovies } from "./utils";
+
 export const getUpcomingMoviesPreview = async () => {
     const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_API_KEY}`);
     const data = await res.json();
@@ -6,67 +8,5 @@ export const getUpcomingMoviesPreview = async () => {
 
     const upcomingPreviewMoviesContainer = document.querySelector('#trending-today .slider-list .slider-inner');
 
-    movies.forEach(movie => {
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('movie-card');
-
-        const movieFigure = document.createElement('figure');
-        movieFigure.classList.add('poster-box');
-        movieFigure.classList.add('card-banner');
-
-        const movieImg = document.createElement('img');
-        movieImg.classList.add('img-cover');
-        movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute(
-            'src',
-            'https://image.tmdb.org/t/p/w300' + movie.poster_path
-            );
-
-        // Titulo 
-        const titleMovie = document.createElement('h4');
-        titleMovie.classList.add('title');
-        titleMovie.textContent = `${movie.title}`;
-
-        // Puntuacion de Pelicula y year
-        const starContainer = document.createElement('div');
-        starContainer.classList.add('meta-list');
-
-        const starItem = document.createElement('div');
-        starItem.classList.add('meta-item');
-
-        const starImg = document.createElement('img');
-        starImg.setAttribute('alt', 'rating');
-        starImg.setAttribute('loading', 'lazy');
-        starImg.setAttribute(
-            'src',
-            '/src/assets/star.png'
-        )
-        starImg.width = 24;
-        starImg.height = 24;
-
-        const starRating = document.createElement('span');
-        starRating.classList.add('span');
-        starRating.textContent = `${movie.vote_average}`;
-
-        const yearContainer = document.createElement('div');
-        yearContainer.classList.add('card-badge');
-        yearContainer.textContent = `${movie.release_date}`;
-
-        starItem.appendChild(starImg);
-        starItem.appendChild(starRating);
-
-        const linkCardBtn = document.createElement('a');
-        linkCardBtn.classList.add('card-btn');
-        linkCardBtn.setAttribute('href','/src/movie_details/movie-details.html');
-        linkCardBtn.setAttribute('title', movie.title);
-
-        movieContainer.appendChild(linkCardBtn);
-
-        movieFigure.appendChild(movieImg);
-        movieContainer.appendChild(movieFigure);
-        movieContainer.appendChild(titleMovie);
-        movieContainer.appendChild(starItem);
-        movieContainer.appendChild(yearContainer);
-        upcomingPreviewMoviesContainer.appendChild(movieContainer);
-    });
+    createMovies(movies, upcomingPreviewMoviesContainer);
 }
--- src/mainPage/trending.js
+++ src/mainPage/trending.js
@@ -1,3 +1,4 @@
+import { createMovies } from "./utils";
 
 
 export const getTrendingMoviesPreview = async () => {
@@ -9,72 +10,7 @@ export const getTrendingMoviesPreview = async () => {
 
     const trendingPreviewMoviesContainer = document.querySelector('.movie-list .slider-list .slider-inner');
 
-    movies.forEach(movie => {
-
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('movie-card');
-
-        const movieFigure = document.createElement('figure');
-        movieFigure.classList.add('poster-box');
-        movieFigure.classList.add('card-banner');
-
-
-        const movieImg = document.createElement('img');
-        movieImg.classList.add('img-cover');
-        movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute(
-            'src',
-            'https://image.tmdb.org/t/p/w300' + movie.poster_path
-            );
-
-        // Titulo 
-        const titleMovie = document.createElement('h4');
-        titleMovie.classList.add('title');
-        titleMovie.textContent = `${movie.title}`;
-
-        // Puntuacion de Pelicula y year
-        const starContainer = document.createElement('div');
-        starContainer.classList.add('meta-list');
-
-        const starItem = document.createElement('div');
-        starItem.classList.add('meta-item');
-
-        const starImg = document.createElement('img');
-        starImg.setAttribute('alt', 'rating');
-        starImg.setAttribute('loading', 'lazy');
-        starImg.setAttribute(
-            'src',
-            '/src/assets/star.png'
-        )
-        starImg.width = 24;
-        starImg.height = 24;
-
-        const starRating = document.createElement('span');
-        starRating.classList.add('span');
-        starRating.textContent = `${movie.vote_average}`;
-
-
-        const yearContainer = document.createElement('div');
-        yearContainer.classList.add('card-badge');
-        yearContainer.textContent = `${movie.release_date}`;
-
-        starItem.appendChild(starImg);
-        starItem.appendChild(starRating);
-
-        const linkCardBtn = document.createElement('a');
-        linkCardBtn.classList.add('card-btn');
-        linkCardBtn.setAttribute('href','/src/movie_details/movie-details.html');
-        linkCardBtn.setAttribute('title', movie.title);
-
-        movieContainer.appendChild(linkCardBtn);
-        
-        movieFigure.appendChild(movieImg);
-        movieContainer.appendChild(movieFigure); 
-        movieContainer.appendChild(titleMovie);
-        movieContainer.appendChild(starItem);
-        movieContainer.appendChild(yearContainer);
-        trendingPreviewMoviesContainer.appendChild(movieContainer);
-    });
+    createMovies(movies, trendingPreviewMoviesContainer);
 
 
 }
