337ba5750d54f767f100743382c5424519c64f9f
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-04-08 14:40:35 -0500

    archivo vite.config.js agregado

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        movieList: resolve(__dirname, 'src/movie_list/movie-list.html'),
+        movieDetails: resolve(__dirname, 'src/movie_details/movie-details.html')
+      }
+    }
+  }
+})
+
