18c49b2e3cf5c3c99e1e8651a40f0a5f1d92be1b
Heider Rey Hernandez <heiderhrnndz@gmail.com>
2023-04-08 15:32:13 -0500

    ultimas modificaciones

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         index: resolve(__dirname, 'index.html'),
         movieList: resolve(__dirname, 'src/movie_list/movie-list.html'),
         movieDetails: resolve(__dirname, 'src/movie_details/movie-details.html')
+        starImg: resolve(__dirname, '/src/assets/star.png')
       }
     }
   }
